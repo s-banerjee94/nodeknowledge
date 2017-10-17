f2a016e3bcdfc2ecc4da867b9073930bf2db034d
Sandeepan Banerjee <shondha.tara94@gmail.com>
2017-10-17 22:12:33 +0530

    load view engine in app.js

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const path = require('path');
+
+//init app
 const app = express();
 
+//load view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+//home route
+app.get('/', function(req, res) {
+    res.render('index');
+});
 
 
-
-
-
+//start sever
 app.listen(3000, function(){
     console.log("Srever running port 3000");
 });
