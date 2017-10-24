9b8c2677f6b5fca0f8368cb002d1e2f2a6eef548
Sandeepan Banerjee <shondha.tara94@gmail.com>
2017-10-24 22:05:31 +0530

    add validation function in app.js  and displayed it in views/layout.pug

--- app.js
+++ app.js
@@ -112,20 +112,33 @@ app.get('/articles/add', function(req, res) {
 
 // add submit post route
 app.post('/articles/add', function(req, res) {
-    let article = new Article();
-    article.title = req.body.title;
-    article.author = req.body.author;
-    article.body = req.body.body;
-
-    article.save(function(err) {
-        if(err) {
-            console.log(err);
-        }
-        else {
-            req.flash('success', 'Article Added');
-            res.redirect('/');
-        }
-    });
+    req.checkBody('title', 'Titel is required').notEmpty();
+    req.checkBody('author', 'Author is required').notEmpty();
+    req.checkBody('body', 'Body is required').notEmpty();
+    //get the error
+    let errors = req.validationErrors();
+    if(errors) {
+        res.render('add_article', {
+            title: "Add Article",
+            errors: errors
+        });
+    }
+    else {
+        let article = new Article();
+        article.title = req.body.title;
+        article.author = req.body.author;
+        article.body = req.body.body;
+    
+        article.save(function(err) {
+            if(err) {
+                console.log(err);
+            }
+            else {
+                req.flash('success', 'Article Added');
+                res.redirect('/');
+            }
+        });
+    }
 });
 
 //load edit form
